ef8ee87368535500b7c1a412be0759e449801bdf
root <benden.alif@gmail.com>
2016-11-12 16:07:47 +0700

    first commit

--- HitungLuas/app/src/main/java/com/hitungluas/alif/hitungluas/MainActivity.java
+++ HitungLuas/app/src/main/java/com/hitungluas/alif/hitungluas/MainActivity.java
@@ -2,10 +2,12 @@ package com.hitungluas.alif.hitungluas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.Touch;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,11 +32,21 @@ public class MainActivity extends AppCompatActivity {
                 String panjang = edtPanjang.getText().toString().trim();
                 String lebar = edtLebar.getText().toString().trim();
 
-                double p = Double.parseDouble(panjang);
-                double l = Double.parseDouble(lebar);
+                if(!panjang.equals("") && !lebar.equals("")) {
+                    double p = Double.parseDouble(panjang);
+                    double l = Double.parseDouble(lebar);
 
-                double luas = p * l;
-                txtLuas.setText("Luas : "+luas);
+                    double luas = p * l;
+                    txtLuas.setText("Luas : "+luas);
+                } else {
+                    if (panjang.equals(""))
+                        Toast.makeText(MainActivity.this, "Nilai Panjang Kosong", Toast.LENGTH_SHORT).show();
+                    if (lebar.equals(""))
+                        Toast.makeText(MainActivity.this, "Nilai Lebar Kosong", Toast.LENGTH_SHORT).show();
+
+                }
+
+
 
             }
         });
